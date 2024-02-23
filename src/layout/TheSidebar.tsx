d4fefa3153e5a30cca6bf6bcdc8b5c6da132fd5b
shabeerali <shabeer3508@gmail.com>
2024-02-23 11:01:11 +0530

    Refactor sidebar component and update team page

--- src/layout/TheSidebar.tsx
+++ src/layout/TheSidebar.tsx
@@ -1,14 +1,13 @@
-import { memo, useEffect, useState } from "react";
+import { memo } from "react";
 import _nav from "./_nav";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import {
 	NavItem,
-	NavItemDivider,
-	NavItemDropdown,
-	NavItemTitle,
+	// NavItemDivider,
+	// NavItemDropdown,
+	// NavItemTitle,
 } from "./SidebarNavItems/NavItems";
 
 import logo from "../assets/hyfy_logo.svg";
@@ -26,19 +25,13 @@ const TheSidebar = ({ onClickClose }: TheSidebarProps) => {
 	const sidebar = useSelector((state: any) => state.SidebarReducer);
 	const minimize = sidebar.minimize;
 
-	let navItems = _nav?.filter((navItm: any) => {
-		if (
-			pb.authStore.model?.role === "employee" &&
-			navItm?.path === "/backlog"
-		)
-			return false;
+	const navItems = _nav?.filter((navItm: any) => {
+		if (pb.authStore.model?.role === "employee" && navItm?.path === "/backlog") return false;
 		else return true;
 	});
 
 	return (
-		<div
-			className={`flex h-screen flex-col ltr:border-r-1 rtl:border-l-1 dark:bg-[#16181D]`}
-		>
+		<div className={`flex h-screen flex-col ltr:border-r-1 rtl:border-l-1 dark:bg-[#16181D]`}>
 			<div
 				className={`flex h-20 flex-shrink-0 relative  border-r flex-col  items-center justify-center  dark:text-white`}
 			>
@@ -50,11 +43,7 @@ const TheSidebar = ({ onClickClose }: TheSidebarProps) => {
 					)}
 				</div>
 				<div className="visible md:invisible absolute  ">
-					<Button
-						variant="outline"
-						className=" w-5 h-5 p-0 rounded-full "
-						onClick={onClickClose}
-					>
+					<Button variant="outline" className=" w-5 h-5 p-0 rounded-full " onClick={onClickClose}>
 						<ChevronLeft />
 					</Button>
 				</div>
@@ -62,37 +51,20 @@ const TheSidebar = ({ onClickClose }: TheSidebarProps) => {
 
 			<div className="   flex-1 overflow-y-auto border-r scrollbar-hide">
 				<div className=" mx-2 -mt-[1px]"></div>
-				<div
-					className={`${
-						minimize ? "items-center p-0 px-0 pt-4" : ""
-					} flex flex-col gap-2 `}
-				>
+				<div className={`${minimize ? "items-center p-0 px-0 pt-4" : ""} flex flex-col gap-2 `}>
 					{navItems.map((item, idx) => {
 						return (
-							<NavItem
-								onClick={() => setIndex(idx)}
-								key={`ni_${idx}`}
-								item={item}
-								minimize={minimize}
-							/>
+							<NavItem onClick={() => setIndex(idx)} key={`ni_${idx}`} item={item} minimize={minimize} />
 						);
 					})}
 				</div>
 			</div>
 			<div className="relative border-r  flex h-11 flex-shrink-0 items-center justify-center  p-5 dark:border-line_dark">
 				<div
-					onClick={() =>
-						dispatch({ type: "minimize", minimize: !minimize })
-					}
-					className={`${
-						!minimize && "right-2"
-					} dark:text-foreground  cursor-pointer text-xs rounded-lg  p-1`}
+					onClick={() => dispatch({ type: "minimize", minimize: !minimize })}
+					className={`${!minimize && "right-2"} dark:text-foreground  cursor-pointer text-xs rounded-lg  p-1`}
 				>
-					{minimize ? (
-						<ChevronRightIcon className="h-4 w-4" />
-					) : (
-						<ChevronLeftIcon className="h-4 w-4" />
-					)}
+					{minimize ? <ChevronRightIcon className="h-4 w-4" /> : <ChevronLeftIcon className="h-4 w-4" />}
 				</div>
 			</div>
 		</div>
--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -1,33 +1,28 @@
 import { useState } from "react";
-import { Label } from "@/components/ui/label"
+import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import HYSearch from "@/components/HYComponents/HYSearch";
 import HYSelect from "@/components/HYComponents/HYSelect";
 import HYAvatar from "@/components/HYComponents/HYAvatar";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import {
-	Card,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { LuMoreVertical } from "react-icons/lu";
 
 const Team = () => {
-
 	const [memberInfo, setMemberInfo] = useState({
 		showSideBar: false,
 		showAddMembers: false,
 		showManageMembers: false,
-	})
+	});
 
 	return (
 		<div className="dark:text-foreground mx-6 h-screen">
 			<div className="gap-3">
 				<Tabs defaultValue="members" className=" ">
-
 					<div className="flex items-center justify-between">
-						<Label className="" htmlFor="team">Teams</Label>
+						<Label className="" htmlFor="team">
+							Teams
+						</Label>
 						<TabsList>
 							<TabsTrigger value="members">Members</TabsTrigger>
 							<TabsTrigger value="performance">Performance</TabsTrigger>
@@ -35,36 +30,40 @@ const Team = () => {
 					</div>
 
 					<TabsContent value="members">
-
 						<div className="flex justify-between items-center my-3 2xl:w-2/3">
 							<div className="flex flex-row gap-3 w-1/3 tems-center">
 								<HYSelect className="w-full" label="Recent" options={["Recent", "Old"]} id={"filter"} />
-								<HYSelect className="w-full" label="Roles" options={["all", "admin", "manager", "employee"]} id={"user"} />
+								<HYSelect
+									className="w-full"
+									label="Roles"
+									options={["all", "admin", "manager", "employee"]}
+									id={"user"}
+								/>
 							</div>
 
 							<div className="flex gap-3">
 								<HYSearch />
 								<Button className="text-white">Add Member</Button>
 							</div>
-						</div >
+						</div>
 
 						<div className="flex flex-col gap-y-3  items-center 2xl:w-2/3 ">
 							<Card className="flex flex-row h-16 justify-between items-center  w-full  ">
 								<div className="flex items-center">
 									<HYAvatar className="size-12 ml-6" url="https://github.com/shadcn.png" />
-									<CardHeader className="gap-y-0" >
+									<CardHeader className="gap-y-0">
 										<CardTitle className="">Roshan</CardTitle>
 										<CardDescription className="">Flutter Dev</CardDescription>
 									</CardHeader>
 								</div>
 								<div className="flex dark:text-slate-400 gap-2 items-center ">
-									<Label className="" htmlFor="email">roshan@gmail.com</Label>
-									<LuMoreVertical className="mr-3 size-7"/>
+									<Label className="" htmlFor="email">
+										roshan@gmail.com
+									</Label>
+									<LuMoreVertical className="mr-3 size-7" />
 								</div>
-
 							</Card>
 						</div>
-
 					</TabsContent>
 				</Tabs>
 			</div>
@@ -73,4 +72,3 @@ const Team = () => {
 };
 
 export default Team;
-
