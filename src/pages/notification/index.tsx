a73395993006374655e77857d0b13f66e7a6ed82
sai vidya <saividya@techgebra.com>
2024-03-12 10:25:30 +0530

    settings ui

--- src/pages/notification/index.tsx
+++ src/pages/notification/index.tsx
@@ -1,124 +1,140 @@
-import { Card } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import HYSelect from '@/components/hy-components/HYSelect'
-import HYSearch from '@/components/hy-components/HYSearch'
-import comments from '@/assets/icons/header-icons/comments.svg'
-import taskdone from '@/assets/icons/header-icons/taskdone.svg'
-import repeatblue from '@/assets/icons/header-icons/repeatblue.svg'
-import repeatyellow from '@/assets/icons/header-icons/repeatyellow.svg'
-
-
-
+import { Card } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import HYSelect from "@/components/hy-components/HYSelect";
+import HYSearch from "@/components/hy-components/HYSearch";
+import icon_comment from "@/assets/icons/header-icons/icon_comment.svg";
+import taskdone from "@/assets/icons/header-icons/taskdone.svg";
+import repeatblue from "@/assets/icons/header-icons/repeatblue.svg";
+import repeatyellow from "@/assets/icons/header-icons/repeatyellow.svg";
 
 const Notification = () => {
-    return (
-        <div className='overflow-y-auto h-full '>
-
-            <div className='flex flex-col px-6  w-full gap-y-6 mb-5  '>
-
-                <div className='flex w-3/5 items-center justify-between mt-8 '>
-                    <Label className="w-36" htmlFor="notification">Notifications</Label>
-                    <HYSelect className="w-36" label="Recent" options={["Recent", "Old"]} id={"filter"} />
-                    <HYSelect className="w-36" label="All types" options={["all", "admin", "manager", "employee"]} id={"user"} />
-                    <HYSearch />
-                </div>
-
-                <div className='flex flex-col gap-y-5'>
-                    <Card className='flex w-3/5 h-28 dark:bg-[#131417]'>
-                        <div className='flex m-5 '>
-                            <div className="flex w-7 h-10 pt-1 ">
-                                <img className="w-5 h-5 " src="/task_icon.svg" alt="Project" />
-                            </div>
-                            <div className='flex flex-col'>
-                                <div className='flex gap-2 items-center'>
-                                    <span className='text-sm '>Task assigned</span>
-                                    <span className=' text-[#737377]'>|</span>
-                                    <span className=" text-sm text-[#737377]">just now</span>
-                                </div>
-                                <span>Task 24 <span className=' text-[#737377]'>of</span> sprint 4</span>
-                                <span className=' text-[#737377] text-sm'>Assigned by <span className='font-medium'>Nuha Fathima</span></span>
-                            </div>
-                        </div>
-
-                    </Card>
-
-                    <Card className='flex w-3/5 h-28 dark:bg-[#131417]'>
-                        <div className='flex m-5 '>
-                            <div className="flex w-7 h-10 pt-1  ">
-                                <img className="w-5 h-5 " src={taskdone} alt="Project" />
-                            </div>
-                            <div className='flex flex-col'>
-                                <div className='flex gap-2 items-center'>
-                                    <span className='text-sm '>Task moved to Done</span>
-                                    <span className=' text-[#737377]'>|</span>
-                                    <span className=" text-sm text-[#737377]">3s ago</span>
-                                </div>
-                                <span>Price API integration <span className=' text-[#737377]'>of</span> sprint 4</span>
-                                <span className=' text-[#737377] text-sm'> By <span className='font-medium'>Roshan</span></span>
-                            </div>
-                        </div>
-
-                    </Card>
-
-                    <Card className='flex w-3/5 h-20 dark:bg-[#131417]'>
-                        <div className='flex my-4 ml-5 '>
-                            <div className="flex w-7 h-10 pt-1   ">
-                                <img className="w-5 h-5 " src={repeatblue} alt="Project" />
-                            </div>
-                            <div className='flex flex-col '>
-                                <div className='flex gap-2 items-center'>
-                                    <span className='text-sm '>Upcoming Sprint</span>
-                                    <span className=' text-[#737377]'>|</span>
-                                    <span className=" text-sm text-[#737377]">9.00 AM</span>
-                                </div>
-                                <span>Sprint 4 <span className=' text-[#737377]'>of </span>Mawasim <span className=' text-[#737377]'>will start </span>Tomorrow</span>
-
-                            </div>
-                        </div>
-
-                    </Card>
-
-                    <Card className='flex w-3/5 h-20 dark:bg-[#131417]'>
-                        <div className='flex ml-5 my-4'>
-                            <div className="flex w-7 h-10 pt-1  ">
-                                <img className="w-5 h-5 " src={repeatyellow} alt="Project" />
-                            </div>
-                            <div className='flex flex-col '>
-                                <div className='flex gap-2 items-center'>
-                                    <span className='text-sm '>Upcoming Sprint Retro</span>
-                                    <span className=' text-[#737377]'>|</span>
-                                    <span className=" text-sm text-[#737377]">22 jan 2023</span>
-                                </div>
-                                <span>Sprint 3 <span className=' text-[#737377]'>of </span>Mawasim <span className=' text-[#737377]'>ends </span>Tomorrow</span>
-
-                            </div>
-                        </div>
-
-                    </Card>
-                    <Card className='flex w-3/5 h-28 dark:bg-[#131417]'>
-                        <div className='flex m-5 '>
-                            <div className="flex w-7 h-10 pt-1 ">
-                                <img className="w-5 h-5 " src={comments} alt="Project" />
-                            </div>
-                            <div className='flex flex-col'>
-                                <div className='flex gap-2 items-center'>
-                                    <span className='text-sm '>Comment</span>
-                                    <span className=' text-[#737377]'>|</span>
-                                    <span className=" text-sm text-[#737377]">22 jan 2023</span>
-                                </div>
-                                <span>"we need to revise the specs"</span>
-                                <span className=' text-[#737377] text-sm'>By <span className='font-medium'>ShabeerAli </span>on story <span className='font-medium'>'Setup Backend'</span></span>
-                            </div>
-                        </div>
-
-                    </Card>
-
-                </div>
-
-            </div>
-
+  return (
+    <div className="overflow-y-auto h-full ">
+      <div className="flex flex-col px-6  w-full gap-y-6 mb-5  ">
+        <div className="flex w-3/5 items-center justify-between mt-8 ">
+          <Label className="w-36" htmlFor="notification">
+            Notifications
+          </Label>
+          <HYSelect
+            className="w-36"
+            label="Recent"
+            options={["Recent", "Old"]}
+            id={"filter"}
+          />
+          <HYSelect
+            className="w-36"
+            label="All types"
+            options={["all", "admin", "manager", "employee"]}
+            id={"user"}
+          />
+          <HYSearch />
         </div>
-    )
-}
 
-export default Notification
+        <div className="flex flex-col gap-y-5">
+          <Card className="flex w-3/5 h-28 dark:bg-[#131417]">
+            <div className="flex m-5 ">
+              <div className="flex w-7 h-10 pt-1 ">
+                <img className="w-5 h-5 " src="/task_icon.svg" alt="Project" />
+              </div>
+              <div className="flex flex-col">
+                <div className="flex gap-2 items-center">
+                  <span className="text-sm ">Task assigned</span>
+                  <span className=" text-[#737377]">|</span>
+                  <span className=" text-sm text-[#737377]">just now</span>
+                </div>
+                <span>
+                  Task 24 <span className=" text-[#737377]">of</span> sprint 4
+                </span>
+                <span className=" text-[#737377] text-sm">
+                  Assigned by <span className="font-medium">Nuha Fathima</span>
+                </span>
+              </div>
+            </div>
+          </Card>
+
+          <Card className="flex w-3/5 h-28 dark:bg-[#131417]">
+            <div className="flex m-5 ">
+              <div className="flex w-7 h-10 pt-1  ">
+                <img className="w-5 h-5 " src={taskdone} alt="Project" />
+              </div>
+              <div className="flex flex-col">
+                <div className="flex gap-2 items-center">
+                  <span className="text-sm ">Task moved to Done</span>
+                  <span className=" text-[#737377]">|</span>
+                  <span className=" text-sm text-[#737377]">3s ago</span>
+                </div>
+                <span>
+                  Price API integration{" "}
+                  <span className=" text-[#737377]">of</span> sprint 4
+                </span>
+                <span className=" text-[#737377] text-sm">
+                  {" "}
+                  By <span className="font-medium">Roshan</span>
+                </span>
+              </div>
+            </div>
+          </Card>
+
+          <Card className="flex w-3/5 h-20 dark:bg-[#131417]">
+            <div className="flex my-4 ml-5 ">
+              <div className="flex w-7 h-10 pt-1   ">
+                <img className="w-5 h-5 " src={repeatblue} alt="Project" />
+              </div>
+              <div className="flex flex-col ">
+                <div className="flex gap-2 items-center">
+                  <span className="text-sm ">Upcoming Sprint</span>
+                  <span className=" text-[#737377]">|</span>
+                  <span className=" text-sm text-[#737377]">9.00 AM</span>
+                </div>
+                <span>
+                  Sprint 4 <span className=" text-[#737377]">of </span>Mawasim{" "}
+                  <span className=" text-[#737377]">will start </span>Tomorrow
+                </span>
+              </div>
+            </div>
+          </Card>
+
+          <Card className="flex w-3/5 h-20 dark:bg-[#131417]">
+            <div className="flex ml-5 my-4">
+              <div className="flex w-7 h-10 pt-1  ">
+                <img className="w-5 h-5 " src={repeatyellow} alt="Project" />
+              </div>
+              <div className="flex flex-col ">
+                <div className="flex gap-2 items-center">
+                  <span className="text-sm ">Upcoming Sprint Retro</span>
+                  <span className=" text-[#737377]">|</span>
+                  <span className=" text-sm text-[#737377]">22 jan 2023</span>
+                </div>
+                <span>
+                  Sprint 3 <span className=" text-[#737377]">of </span>Mawasim{" "}
+                  <span className=" text-[#737377]">ends </span>Tomorrow
+                </span>
+              </div>
+            </div>
+          </Card>
+          <Card className="flex w-3/5 h-28 dark:bg-[#131417]">
+            <div className="flex m-5 ">
+              <div className="flex w-7 h-10 pt-1 ">
+                <img className="w-5 h-5 " src={icon_comment} alt="Project" />
+              </div>
+              <div className="flex flex-col">
+                <div className="flex gap-2 items-center">
+                  <span className="text-sm ">Comment</span>
+                  <span className=" text-[#737377]">|</span>
+                  <span className=" text-sm text-[#737377]">22 jan 2023</span>
+                </div>
+                <span>"we need to revise the specs"</span>
+                <span className=" text-[#737377] text-sm">
+                  By <span className="font-medium">ShabeerAli </span>on story{" "}
+                  <span className="font-medium">'Setup Backend'</span>
+                </span>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Notification;
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,11 +1,115 @@
+import { Label } from "@/components/ui/label";
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import icons_dwnld from "@/assets/icons/header-icons/icons_dwnld.svg";
+import icon_arrow from "@/assets/icons/header-icons/icon_arrow.svg";
+import icon_org from "@/assets/icons/header-icons/icon_org.svg";
+import icons_billing from "@/assets/icons/header-icons/Icons_billing.svg";
+import icon_settingsnotification from "@/assets/icons/header-icons/Icons_settingsnotification.svg";
+import icon_userRoles from "@/assets/icons/header-icons/Icons_userRoles.svg";
 
-const index = () => {
-	return (
-		<div className="dark:text-foreground flex justify-center h-full items-center">
-			Settings
-		</div>
-	);
+const Settings = () => {
+  return (
+    <div className="dark:text-foreground flex flex-col m-8 gap-y-4">
+      <Label htmlFor="">Settings</Label>
+
+      <div className="flex flex-col gap-5">
+        <Card className="flex w-4/6 h-16 dark:bg-[#131417]">
+          <div className="flex w-full items-center">
+            <img className=" w-5 h-5 m-5  " src={icon_org} alt="org_icon" />
+            <div className="flex flex-row gap-20 items-center">
+              <Label className="flex w-20 text-[#737377] text-sm">
+                Organization
+              </Label>
+              <Label className=" text-sm flex gap-5">
+                <span className="text-[#737377] text-sm">Active</span>
+                <span>ABC Software LLP</span>
+              </Label>
+            </div>
+          </div>
+          <div className="flex ">
+            <img className=" justify-items-end" src={icon_arrow} alt=""></img>
+          </div>
+        </Card>
+
+        <Card className="flex w-4/6 h-16 dark:bg-[#131417]">
+          <div className="flex w-full items-center">
+            <img className="flex w-5 h-5 m-5  " src={icons_billing} alt="" />
+            <div className="flex flex-row gap-20 items-center">
+              <Label className="flex w-20 text-[#737377] text-sm">
+                Billing
+              </Label>
+              <Label className=" text-sm flex gap-2">
+                <span className="text-sm">Platimum</span>
+                <span className="text-[#737377]">|</span>
+                <span className="text-[#737377]">Plan valid till</span>
+                <span className="text-[#737377]">24 jun 2024</span>
+              </Label>
+            </div>
+            <div className="flex gap-3 pl-16">
+              <Button
+                className="w-32 text-[#737377]"
+                type="reset"
+                variant="outline"
+              >
+                <img className="p-3" src={icons_dwnld} alt=""></img> Invoice
+              </Button>
+              <Button
+                className="border-sky-700 w-32 text-sky-700"
+                type="reset"
+                variant="outline"
+              >
+                Upgrade
+              </Button>
+            </div>
+          </div>
+          <div className="flex ">
+            <img className=" justify-items-end" src={icon_arrow} alt=""></img>
+          </div>
+        </Card>
+
+        <Card className="flex w-4/6 h-16 dark:bg-[#131417]">
+          <div className="flex w-full items-center">
+            <img
+              className=" w-5 h-5 m-5  "
+              src={icon_settingsnotification}
+              alt="org_icon"
+            />
+            <div className="flex flex-row gap-20 items-center">
+              <Label className="text-[#737377] text-sm">Notifications</Label>
+            </div>
+          </div>
+          <div className="flex ">
+            <img className=" justify-items-end" src={icon_arrow} alt=""></img>
+          </div>
+        </Card>
+
+        <Card className="flex w-4/6 h-16 dark:bg-[#131417]">
+          <div className="flex w-full items-center">
+            <img
+              className=" w-5 h-5 m-5  "
+              src={icon_userRoles}
+              alt="org_icon"
+            />
+            <div className="flex flex-row gap-20 items-center">
+              <Label className="text-[#737377] text-sm">User Roles</Label>
+            </div>
+          </div>
+          <div className="flex ">
+            <img className=" justify-items-end" src={icon_arrow} alt=""></img>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
-export default index;
+export default Settings;
